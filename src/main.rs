bfbd2b89d8f78c437586e78d54b2c46c2d6183fe
Kyle Pfromer <kpfromer2@gmail.com>
2022-11-09 12:36:53 -0700

    accel/gyro

--- src/main.rs
+++ src/main.rs
@@ -3,182 +3,192 @@
 
 use panic_semihosting as _;
 
-use stm32f3xx_hal as hal;
-use ws2812_spi as ws2812;
+use stm32f3xx_hal as _;
 
-use cortex_m_rt::entry;
+extern crate stm32f3xx_hal as hal;
 
-use cortex_m_semihosting::hprintln;
+use rtic::app;
+use rtt_target::{rprintln, rtt_init_print};
+use stm32f3xx_hal::gpio::{Output, PushPull, PE10};
+use stm32f3xx_hal::prelude::*;
+use systick_monotonic::{fugit::Duration, Systick};
 
-use smart_leds::colors;
-use smart_leds::RGB;
+use lsm303dlhc::Lsm303dlhc;
 
-use hal::flash::FlashExt;
-use hal::gpio::marker::Gpio;
-use hal::gpio::marker::Index;
-use hal::gpio::GpioExt;
-use hal::gpio::Output;
-use hal::gpio::Pin;
-use hal::gpio::PushPull;
-use hal::pac::Peripherals;
-use hal::prelude::*;
-use hal::rcc::RccExt;
-use hal::spi::Spi;
+use l3gd20::L3gd20;
 
-use ws2812::Ws2812;
+// Referenced https://github.com/rtic-rs/rtic-examples/tree/master/rtic_v1/stm32f3_blinky/src
+#[app(device = stm32f3xx_hal::pac, peripherals = true, dispatchers = [SPI1])]
+mod app {
+    use cortex_m_semihosting::hprintln;
 
-use smart_leds::SmartLedsWrite;
+    use super::*;
 
-// struct Pins {
-//     pub north: Pin<Gpio, Index, Output<PushPull>>,
-// }
+    #[shared]
+    struct Shared {}
 
-#[entry]
-fn main() -> ! {
-    let dp = Peripherals::take().unwrap();
-    let mut rcc = dp.RCC.constrain();
-    let mut acr = dp.FLASH.constrain().acr;
-    let mut gpioa = dp.GPIOA.split(&mut rcc.ahb);
-    let mut gpiob = dp.GPIOB.split(&mut rcc.ahb);
-    let mut gpioe = dp.GPIOE.split(&mut rcc.ahb);
-
-    let clocks = rcc
-        .cfgr
-        .use_hse(8.MHz())
-        .sysclk(48.MHz())
-        .pclk1(24.MHz())
-        .pclk2(24.MHz())
-        .freeze(&mut acr);
-
-    // Set LEDs
-
-    let sck =
-        gpiob
-            .pb3
-            .into_af_push_pull::<5>(&mut gpiob.moder, &mut gpiob.otyper, &mut gpiob.afrl);
-    let miso =
-        gpiob
-            .pb4
-            .into_af_push_pull::<5>(&mut gpiob.moder, &mut gpiob.otyper, &mut gpiob.afrl);
-    let mosi =
-        gpiob
-            .pb5
-            .into_af_push_pull::<5>(&mut gpiob.moder, &mut gpiob.otyper, &mut gpiob.afrl);
-    // let sck =
-    //     gpioa
-    //         .pa5
-    //         .into_af_push_pull::<5>(&mut gpioa.moder, &mut gpioa.otyper, &mut gpioa.afrl);
-    // let miso =
-    //     gpioa
-    //         .pa6
-    //         .into_af_push_pull::<5>(&mut gpioa.moder, &mut gpioa.otyper, &mut gpioa.afrl);
-    // let mosi =
-    //     gpioa
-    //         .pa7
-    //         .into_af_push_pull::<5>(&mut gpioa.moder, &mut gpioa.otyper, &mut gpioa.afrl);
-
-    let spi = Spi::new(dp.SPI1, (sck, miso, mosi), 3.MHz(), clocks, &mut rcc.apb2);
-
-    let mut ws = Ws2812::new(spi);
-    let mut data = [colors::WHITE; 60];
-    for i in 0..data.len() {
-        data[i] = RGB { r: 0, g: 0, b: 0 }
-        // data[i] = hsv2rgb(Hsv {
-        //     hue: (i as u8) * 32,
-        //     sat: 255,
-        //     val: 32,
-        // });
-    }
-    ws.write(data.iter().cloned()).unwrap();
-    hprintln!("Written");
-
-    let button = gpioa.pa0.into_input(&mut gpioa.moder);
-
-    let mut north = gpioe
-        .pe9
-        .into_push_pull_output(&mut gpioe.moder, &mut gpioe.otyper);
-    let mut north_east = gpioe
-        .pe10
-        .into_push_pull_output(&mut gpioe.moder, &mut gpioe.otyper);
-    let mut east = gpioe
-        .pe11
-        .into_push_pull_output(&mut gpioe.moder, &mut gpioe.otyper);
-    let mut south_east = gpioe
-        .pe12
-        .into_push_pull_output(&mut gpioe.moder, &mut gpioe.otyper);
-    let mut south = gpioe
-        .pe13
-        .into_push_pull_output(&mut gpioe.moder, &mut gpioe.otyper);
-    let mut south_west = gpioe
-        .pe14
-        .into_push_pull_output(&mut gpioe.moder, &mut gpioe.otyper);
-    let mut west = gpioe
-        .pe15
-        .into_push_pull_output(&mut gpioe.moder, &mut gpioe.otyper);
-    let mut north_west = gpioe
-        .pe8
-        .into_push_pull_output(&mut gpioe.moder, &mut gpioe.otyper);
-
-    let mut leds = (
-        north, north_east, east, south_east, south, south_west, west, north_west,
-    );
-
-    let mut show = false;
-    let mut i = 0;
-    loop {
-        if button.is_high().unwrap() {
-            show = !show;
-        }
-
-        if show {
-            leds.0.set_low().unwrap();
-            leds.1.set_low().unwrap();
-            leds.2.set_low().unwrap();
-            leds.3.set_low().unwrap();
-            leds.4.set_low().unwrap();
-            leds.5.set_low().unwrap();
-            leds.6.set_low().unwrap();
-            leds.7.set_low().unwrap();
-
-            match i {
-                0 => leds.0.set_high().unwrap(),
-                1 => leds.1.set_high().unwrap(),
-                2 => leds.2.set_high().unwrap(),
-                3 => leds.3.set_high().unwrap(),
-                4 => leds.4.set_high().unwrap(),
-                5 => leds.5.set_high().unwrap(),
-                6 => leds.6.set_high().unwrap(),
-                7 => leds.7.set_high().unwrap(),
-                _ => panic!("Invalid pin"),
-            }
-            if i == 7 {
-                i = 0;
-            } else {
-                i += 1;
-            }
-            cortex_m::asm::delay(8_000_000);
-        }
-        // if button.is_high().unwrap() {
-        //     east.set_high().unwrap();
-        //     cortex_m::asm::delay(8_000_000);
-        // } else {
-        //     east.set_low().unwrap();
-        // }
-        // cortex_m::asm::delay(8_000_000);
-
-        // led.toggle().unwrap();
+    #[local]
+    struct Local {
+        led: PE10<Output<PushPull>>,
+        state: bool,
+        magnetometer_sensor: lsm303dlhc::Lsm303dlhc<
+            hal::i2c::I2c<
+                stm32f3xx_hal::pac::I2C1,
+                (
+                    hal::gpio::Pin<
+                        hal::gpio::Gpiob,
+                        hal::gpio::U<6>,
+                        hal::gpio::Alternate<hal::gpio::OpenDrain, 4>,
+                    >,
+                    hal::gpio::Pin<
+                        hal::gpio::Gpiob,
+                        hal::gpio::U<7>,
+                        hal::gpio::Alternate<hal::gpio::OpenDrain, 4>,
+                    >,
+                ),
+            >,
+        >,
+        gyroscope_sensor: L3gd20<
+            hal::spi::Spi<
+                stm32f3xx_hal::pac::SPI1,
+                (
+                    hal::gpio::Pin<
+                        hal::gpio::Gpioa,
+                        hal::gpio::U<5>,
+                        hal::gpio::Alternate<PushPull, 5>,
+                    >,
+                    hal::gpio::Pin<
+                        hal::gpio::Gpioa,
+                        hal::gpio::U<6>,
+                        hal::gpio::Alternate<PushPull, 5>,
+                    >,
+                    hal::gpio::Pin<
+                        hal::gpio::Gpioa,
+                        hal::gpio::U<7>,
+                        hal::gpio::Alternate<PushPull, 5>,
+                    >,
+                ),
+            >,
+            hal::gpio::Pin<hal::gpio::Gpioe, hal::gpio::U<3>, Output<PushPull>>,
+        >,
     }
 
-    // loop {
-    //     leds.ld3.toggle().ok();
-    //     delay.delay_ms(1000u16);
-    //     leds.ld3.toggle().ok();
-    //     delay.delay_ms(1000u16);
+    #[monotonic(binds = SysTick, default = true)]
+    type MonoTimer = Systick<1000>;
 
-    //     //explicit on/off
-    //     leds.ld4.on().ok();
-    //     delay.delay_ms(1000u16);
-    //     leds.ld4.off().ok();
-    //     delay.delay_ms(1000u16);
-    // }
+    #[init]
+    fn init(cx: init::Context) -> (Shared, Local, init::Monotonics) {
+        // Setup clocks
+        let mut flash = cx.device.FLASH.constrain();
+        let mut rcc = cx.device.RCC.constrain();
+
+        let mono = Systick::new(cx.core.SYST, 36_000_000);
+
+        rtt_init_print!();
+        rprintln!("init");
+
+        let clocks = rcc
+            .cfgr
+            .use_hse(8.MHz())
+            .sysclk(36.MHz())
+            .pclk1(36.MHz())
+            .freeze(&mut flash.acr);
+
+        // Setup LED
+        let mut gpioe = cx.device.GPIOE.split(&mut rcc.ahb);
+        let mut led = gpioe
+            .pe10
+            .into_push_pull_output(&mut gpioe.moder, &mut gpioe.otyper);
+        led.set_high().unwrap();
+
+        // Setup mag
+        let mut gpiob = cx.device.GPIOB.split(&mut rcc.ahb);
+        let scl =
+            gpiob
+                .pb6
+                .into_af4_open_drain(&mut gpiob.moder, &mut gpiob.otyper, &mut gpiob.afrl);
+        // .into_open_drain_output(&mut gpiob.moder, &mut gpiob.otyper);
+        let sda =
+            gpiob
+                .pb7
+                .into_af4_open_drain(&mut gpiob.moder, &mut gpiob.otyper, &mut gpiob.afrl);
+        let i2c = hal::i2c::I2c::new(
+            cx.device.I2C1,
+            (scl, sda),
+            400_000.Hz(),
+            clocks,
+            &mut rcc.apb1,
+        );
+        let magnetometer_sensor = Lsm303dlhc::new(i2c).unwrap();
+
+        let mut gpioa = cx.device.GPIOA.split(&mut rcc.ahb);
+        let sck =
+            gpioa
+                .pa5
+                .into_af5_push_pull(&mut gpioa.moder, &mut gpioa.otyper, &mut gpioa.afrl);
+        let miso =
+            gpioa
+                .pa6
+                .into_af5_push_pull(&mut gpioa.moder, &mut gpioa.otyper, &mut gpioa.afrl);
+        let mosi =
+            gpioa
+                .pa7
+                .into_af5_push_pull(&mut gpioa.moder, &mut gpioa.otyper, &mut gpioa.afrl);
+        let nss = gpioe
+            .pe3
+            .into_push_pull_output(&mut gpioe.moder, &mut gpioe.otyper);
+        let spi = hal::spi::Spi::new(
+            cx.device.SPI1,
+            (sck, miso, mosi),
+            1.MHz(),
+            clocks,
+            &mut rcc.apb2,
+        );
+        let gyroscope_sensor = L3gd20::new(spi, nss).unwrap();
+
+        // Schedule the blinking task
+        blink::spawn_after(Duration::<u64, 1, 1000>::from_ticks(1000)).unwrap();
+        // Schedule magnetometer task
+        magnetometer::spawn_after(Duration::<u64, 1, 1000>::from_ticks(1000)).unwrap();
+        // Schedule gyro task
+        gyroscope::spawn_after(Duration::<u64, 1, 1000>::from_ticks(1000)).unwrap();
+
+        (
+            Shared {},
+            Local {
+                led,
+                state: false,
+                magnetometer_sensor,
+                gyroscope_sensor,
+            },
+            init::Monotonics(mono),
+        )
+    }
+
+    #[task(local = [led, state])]
+    fn blink(cx: blink::Context) {
+        rprintln!("blink");
+        if *cx.local.state {
+            cx.local.led.set_high().unwrap();
+            *cx.local.state = false;
+        } else {
+            cx.local.led.set_low().unwrap();
+            *cx.local.state = true;
+        }
+        blink::spawn_after(Duration::<u64, 1, 1000>::from_ticks(1000)).unwrap();
+    }
+
+    #[task(local = [magnetometer_sensor])]
+    fn magnetometer(cx: magnetometer::Context) {
+        let lsm303dlhc::I16x3 { x, y, z } = cx.local.magnetometer_sensor.accel().unwrap();
+        // hprintln!("{} {} {}", x, y, z);
+        magnetometer::spawn_after(Duration::<u64, 1, 1000>::from_ticks(1000)).unwrap();
+    }
+
+    #[task(local = [gyroscope_sensor])]
+    fn gyroscope(cx: gyroscope::Context) {
+        let l3gd20::I16x3 { x, y, z } = cx.local.gyroscope_sensor.gyro().unwrap();
+        hprintln!("{} {} {}", x, y, z);
+        gyroscope::spawn_after(Duration::<u64, 1, 1000>::from_ticks(1000)).unwrap();
+    }
 }
